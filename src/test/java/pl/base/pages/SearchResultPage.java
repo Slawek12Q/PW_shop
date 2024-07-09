5c3286b2506038afcb3f93a71612a93f7970be6a
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-07-09 23:50:39 +0200

    waiting for page load was added

--- src/test/java/pl/base/pages/SearchResultPage.java
+++ src/test/java/pl/base/pages/SearchResultPage.java
@@ -1,8 +1,12 @@
 package pl.base.pages;
 
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.LoadState;
 import lombok.Getter;
+import org.example.utils.PageUtils;
 import pl.base.pages.sections.searchResultsSection.SearchResultSection;
+
+import static org.example.utils.PageUtils.*;
 
 @Getter
 public class SearchResultPage {
@@ -10,6 +14,7 @@ public class SearchResultPage {
     private SearchResultSection searchResultSection;
 
     public SearchResultPage(Page page) {
+        waitForLoadState(page);
         this.searchResultSection = new SearchResultSection(page);
     }
 }
