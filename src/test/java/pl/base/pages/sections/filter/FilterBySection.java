836f604f3e246ff70d0a19789a0582bd3172bd4d
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-07-18 00:46:44 +0200

    New section was added

--- src/test/java/pl/base/pages/sections/filter/FilterBySection.java
+++ src/test/java/pl/base/pages/sections/filter/FilterBySection.java
@@ -1,4 +1,20 @@
 package pl.base.pages.sections.filter;
 
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
 public class FilterBySection {
+
+    private Locator leftSlider;
+
+    public FilterBySection(Page page) {
+        this.leftSlider = page.locator(".ui-slider-handle").first();
+    }
+
+    public void showLeftLocator() {
+        System.out.println(leftSlider.boundingBox().x);
+        System.out.println(leftSlider.boundingBox().y);
+        System.out.println(leftSlider.boundingBox().height);
+        System.out.println(leftSlider.boundingBox().width);
+    }
 }
