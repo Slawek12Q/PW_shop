a980cddff0be0a396b2d9aeeac40ee1bc3022e31
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-08-21 23:56:41 +0200

    Code refactor home page

--- src/test/java/pl/base/pages/HomePage.java
+++ src/test/java/pl/base/pages/HomePage.java
@@ -2,18 +2,12 @@ package pl.base.pages;
 
 import com.microsoft.playwright.Page;
 import lombok.Getter;
-import pl.base.pages.sections.TopMenuAndSearchSection;
-import pl.base.pages.sections.TopNavigationSection;
 
 @Getter
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
 
-    private TopMenuAndSearchSection topMenuAndSearchSection;
-    private TopNavigationSection topNavigationSection;
 
     public HomePage(Page page) {
         super(page);
-        this.topMenuAndSearchSection = new TopMenuAndSearchSection(page);
-        this.topNavigationSection = new TopNavigationSection(page);
     }
 }
